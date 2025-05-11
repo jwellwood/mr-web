18551bd08b7baf3b1bf95532c64fc6891bc7e817
quirkles <al.quirk@gmail.com>
2025-05-10 20:54:09 -0400

    106

--- src/modules/home/components/SearchForm.tsx
+++ src/modules/home/components/SearchForm.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useForm } from 'react-hook-form';
 import { FormContainer } from '../../../components/containers';
 import ControlledTextInput from '../../../components/inputs/ControlledTextInput';
